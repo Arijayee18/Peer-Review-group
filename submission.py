3d17318e23cdebfc95cadab722168d33176fae4a
cjalcorn <146035481+cjalcorn@users.noreply.github.com>
2023-09-25 15:42:51 -0600

    Add files via upload

--- submission.py
+++ submission.py
@@ -1,66 +1,47 @@
-'''
-Ari'Jaye Derritt
-CSCI 128 - Section J
-Assessment 4
-References: no one
-time: 1 hour 
-'''
-p1 = input("P1> ")
-p2 = input("P2> ")
-p3 = input("P3> ")
+# Cameron Alcorn
+# CSCI 128 Section J
+# Assessment 4
+# References: None
+# Time: 90 mins
+coord1 = input("COORD1>").split()
+coord1[0] = float(coord1[0])
+coord1[1] = float(coord1[1])
 
-#spliiting the string to get x and y coordinates
-p1 = p1.split()
-p2 = p2.split()
-p3 = p3.split()
+coord2 = input("COORD2>").split()
+coord2[0] = float(coord2[0])
+coord2[1] = float(coord2[1])
 
-#x and y coordinates 
-p1x = float(p1[0])
-p1y = float(p1[1])
-p2x = float(p2[0])
-p2y = float(p2[1])
-p3x = float(p3[0])
-p3y = float(p3[1])
+coord3 = input("COORD3>").split()
+coord3[0] = float(coord3[0])
+coord3[1] = float(coord3[1])
 
-# Step 1: Find the side lengths 
-side1 = round((((p2x-p1x)**2)+(p2y-p1y)**2)**0.5, 4)
-side2 = round((((p3x-p2x)**2)+(p3y-p2y)**2)**0.5, 4)
-side3 = round((((p3x-p1x)**2)+(p3y-p1y)**2)**0.5, 4)
-sideList = [side1, side2, side3]
-sideList.sort()
-print(f'OUTPUT {sideList}')
+dist12 = round((((coord1[0] - coord2[0]) ** 2) + ((coord1[1] - coord2[1]) ** 2)) ** 0.5, 4)
+dist13 = round((((coord1[0] - coord3[0]) ** 2) + ((coord1[1] - coord3[1]) ** 2)) ** 0.5, 4)
+dist23 = round((((coord2[0] - coord3[0]) ** 2) + ((coord2[1] - coord3[1]) ** 2)) ** 0.5, 4)
 
-#Step 2: determine whether the points are duplicate or collinear, then the type of triangle
+dist_list = [dist12, dist13, dist23]
+dist_list.sort()
+print(f'OUTPUT {dist_list}')
 
-if p1 == p2 or p1 == p3 or p2==p3:
-    print('OUTPUT Duplicate Point')
-elif sideList[0] + sideList[1] == sideList[2]:
-    print('OUTPUT Collinear')
-elif sideList[0]**2 + sideList[1]**2 < sideList[2]**2: #obstuse triangle
-    if sideList[0]==sideList[1] or sideList[1]==sideList[2] or sideList[0]==sideList[2]:
-        print('OUTPUT Obtuse Isosceles Triangle')
-    elif sideList[0] != sideList[1] and sideList[1]!=sideList[2] and sideList[0]!=sideList[2]:
-        print('OUTPUT Obtuse Scalene Triangle')
-    else:
-        print('OUTPUT Oblique Triangle')
-elif sideList[0]**2 + sideList[1]**2 == sideList[2]**2: #right triangle
-    if sideList[0] != sideList[1] and sideList[1] != sideList[2] and sideList[0] != sideList[2]:
-        print('OUTPUT Right Scalene Triangle')
-    if sideList[0]==sideList[1]or sideList[1]==sideList[2] or sideList[0]==sideList[2]:
-        print('OUTPUT Isosceles Triangle')
-elif sideList[0]**2 + sideList[1]**2 > sideList[2]**2: #acute triangle
-    if side1 == side2 == side3:
-        print('OUTPUT Acute Equilateral Triangle')
-    elif sideList[0] == sideList[1] or sideList[1] == sideList[2] or sideList[0] == sideList[2]:
-        print('OUTPUT Acute Isosceles Triangle')
-    elif sideList[0] != sideList[1] and sideList[1] != sideList[2] and sideList[0] != sideList[2]:
-        print('OUTPUT Acute Scalene Triangle')
-    else:
-        print('OUTPUT Oblique Triangle')
-        
+if dist12 == 0 or dist13 == 0 or dist23 == 0:
+    print("OUTPUT Duplicate Point")
+elif dist_list[0] + dist_list[1] == dist_list[2]:
+    print("OUTPUT Collinear")
 
+angle_class = (dist_list[0]) ** 2 + (dist_list[1]) ** 2
+if angle_class < (dist_list[2]) ** 2:
+    angle = "Obtuse"
+elif angle_class == (dist_list[2]) ** 2:
+    angle = "Right"
+else:
+    angle = "Acute"
 
+if dist12 == dist13 and dist12 == dist23:
+    length = "Equilateral"
+elif dist12 == dist13 or dist12 == dist23 or dist13 == dist23:
+    length = "Isosceles"
+else:
+    length = "Scalene"
 
-
-
-
+if dist12 != 0 and dist13 != 0 and dist23 != 0 and dist_list[0] + dist_list[1] != dist_list[2]:    
+    print("OUTPUT", angle, length, "Triangle")
